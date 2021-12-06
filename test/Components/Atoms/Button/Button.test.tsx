4f0b6a18ef7c576ddf12193e8605b31436e39d65
Bayu Pranoto Dwi Putra <pranoto@Bayus-MacBook-Air.local>
2021-12-06 19:20:02 +0700

    fix ci cd chromatic 2

--- test/Components/Atoms/Button/Button.test.tsx
+++ test/Components/Atoms/Button/Button.test.tsx
@@ -42,28 +42,4 @@ describe("FAButton", () => {
 
     expect(number).toBe(2);
   });
-
-  it("should render gradient blue button by gradientColor property", () => {
-    const { getByTestId } = render(
-      <FAButton testID="button-example" gradientColor="blue">
-        FAButton
-      </FAButton>
-    );
-
-    expect(getByTestId("button-example")).toHaveStyle(
-      "background: linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)"
-    );
-  });
-
-  it("should render gradient red button by gradientColor property", () => {
-    const { getByTestId } = render(
-      <FAButton testID="button-example" gradientColor="red">
-        FAButton
-      </FAButton>
-    );
-
-    expect(getByTestId("button-example")).toHaveStyle(
-      "background: linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
-    );
-  });
 });
