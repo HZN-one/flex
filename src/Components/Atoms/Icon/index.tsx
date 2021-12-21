94fee916d7fe7ce60e14dad80e5aebcc00fbfd1c
Bayu Pranoto Dwi Putra <pranoto@Bayus-MacBook-Air.local>
2021-12-21 10:04:38 +0700

    configuring atoms icon and logo

--- src/Components/Atoms/Icon/index.tsx
+++ src/Components/Atoms/Icon/index.tsx
@@ -2,10 +2,6 @@ import React, { memo } from "react";
 import { Icon } from "@mui/material";
 import { IIcon } from "./Icon.interface";
 
-// it is mandatory to add these lines in html in order for the icon to appear
-// <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-// <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons+Two+Tone"/>;
-
 export const FAIcon = memo((props: IIcon) => {
   const { testID, ...materialUIProps } = props;
   return <Icon data-testid={testID} {...materialUIProps} />;
