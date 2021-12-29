67cd5d881538a6af59c642e2276791b587e1d8e8
Bayu Pranoto Dwi Putra <pranoto@Bayus-MacBook-Air.local>
2021-12-29 09:07:36 +0700

    fix lint

--- test/Components/Molecules/FMFormTextField/FMFormTextField.test.tsx
+++ test/Components/Molecules/FMFormTextField/FMFormTextField.test.tsx
@@ -2,17 +2,17 @@
  * @jest-environment jsdom
  */
 
-import * as React from 'react'
-import '@testing-library/jest-dom'
-import { FMFormTextField } from 'Components'
+import * as React from "react";
+import "@testing-library/jest-dom";
+import { FMFormTextField } from "Components";
 
-import { render } from '@testing-library/react'
+import { render } from "@testing-library/react";
 
-describe('FMFormTextField', () => {
-  it('should match snapshot', () => {
+describe("FMFormTextField", () => {
+  it("should match snapshot", () => {
     const { container } = render(
       <FMFormTextField variant="outlined" testID="input-example" />
-    )
-    expect(container).toMatchSnapshot()
-  })
-})
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
