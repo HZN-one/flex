4c60572deabb139f22c90d0bc0e45fc7ec3e8f46
Bayu Pranoto Dwi Putra <pranoto@Bayus-MacBook-Air.local>
2021-12-30 12:59:37 +0700

    publisch chromatic error

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,22 @@
-const path = require('path');
+// const path = require('path');
 
 module.exports = {
-  webpackFinal: async (config) => {
-    config.resolve.modules = [
-      ...(config.resolve.modules || []),
-      path.resolve(__dirname, "../src"),
-    ];
+  // webpackFinal: async (config) => {
+  //   config.resolve.modules = [
+  //     ...(config.resolve.modules || []),
+  //     path.resolve(__dirname, "../src"),
+  //   ];
 
-    config.resolve.alias = {
-      "@Atoms": path.resolve(__dirname, "../src/Components/Atoms"),
-      "@Molecules": path.resolve(__dirname, "../src/Components/Molecules"),
-      "@Organisms": path.resolve(__dirname, "../src/Components/Organisms"),
-      "@src": path.resolve(__dirname, ".."),
-      "@Components": path.resolve(__dirname, "../src/Components")
-   }
+  //   config.resolve.alias = {
+  //     "@Atoms": path.resolve(__dirname, "../src/Components/Atoms"),
+  //     "@Molecules": path.resolve(__dirname, "../src/Components/Molecules"),
+  //     "@Organisms": path.resolve(__dirname, "../src/Components/Organisms"),
+  //     "@src": path.resolve(__dirname, ".."),
+  //     "@Components": path.resolve(__dirname, "../src/Components")
+  //  }
 
-    return config;
-  },
+  //   return config;
+  // },
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
