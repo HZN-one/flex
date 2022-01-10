e4c0a0e1288cbc31a1b85a4da738f505eea65f77
Bayu Pranoto Dwi Putra <pranoto@Bayus-MacBook-Air.local>
2022-01-10 22:07:29 +0700

    configure FMCard with title props

--- src/Components/Molecules/FMCard/index.tsx
+++ src/Components/Molecules/FMCard/index.tsx
@@ -4,12 +4,26 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 
 import { IFMCard } from "./FMCard.interfaces";
+import { FATypography } from "@Atoms/FATypography";
 
 export const FMCard = memo((props: IFMCard) => {
-  const { testID, actions, ...materialUIProps } = props;
+  const { testID, actions, title, ...materialUIProps } = props;
   return (
     <Card data-testid={testID} {...materialUIProps}>
-      <CardContent>{props.children}</CardContent>
+      <CardContent>
+        {title && (
+          <>
+            <FATypography
+              testID="typography-card"
+              variant="h6"
+              fontWeight={700}
+            >
+              {title}
+            </FATypography>
+          </>
+        )}
+        {props.children}
+      </CardContent>
       {actions && <CardActions>{actions}</CardActions>}
     </Card>
   );
