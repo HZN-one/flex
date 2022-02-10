ad6c41814ec194f34097721a64eb1152894b78e8
Muhammad Farhan <muhammadfarhan@Muhammads-MacBook-Pro.local>
2022-02-10 15:54:08 +0700

    embbeded theme and add more color

--- src/Definitions/Theme/Theme.interface.tsx
+++ src/Definitions/Theme/Theme.interface.tsx
@@ -38,6 +38,35 @@ export interface ThemeProps extends ThemeOptions {
         dark?: string;
         light?: string;
       };
+      action: {
+        main?: string;
+        hover?: string;
+        selected?: string;
+      };
+      grey?: {
+        50?: string;
+        100?: string;
+        200?: string;
+        300?: string;
+        400?: string;
+        500?: string;
+        600?: string;
+        700?: string;
+        800?: string;
+        900?: string;
+      };
+      bluegrey?: {
+        50?: string;
+        100?: string;
+        200?: string;
+        300?: string;
+        400?: string;
+        500?: string;
+        600?: string;
+        700?: string;
+        800?: string;
+        900?: string;
+      };
       // Used by `getContrastText()` to maximize the contrast between
       // the background and the text.
       contrastThreshold?: number;
