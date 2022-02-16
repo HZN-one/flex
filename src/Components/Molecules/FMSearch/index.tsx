5c86550e614c601956276b11a2e6f963280d8110
Bryan Lumbantobing <bryantobing0@gmail.com>
2022-02-16 11:41:43 +0700

    Update FMSearch import

--- src/Components/Molecules/FMSearch/index.tsx
+++ src/Components/Molecules/FMSearch/index.tsx
@@ -1,9 +1,8 @@
 import React, { memo } from "react";
 import { Box, InputAdornment } from "@mui/material";
-import { FAIcon } from "@Atoms/Icon";
+import { FAIcon, FATextField } from "@Atoms";
 
 import { IFMSearch } from "./FMSearch.interfaces";
-import { FATextField } from "@Atoms/FATextField";
 
 export const FMSearch = memo((props: IFMSearch) => {
   const { testID, ...materialUIProps } = props;
