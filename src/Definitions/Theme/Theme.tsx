e7c3d54509b4553ab707ad82c977e2ca2dbd4ce9
gilangdarmawan <jobs.gilangdarmawan@gmail.com>
2022-02-16 12:37:57 +0700

    Update Flex Layout and inititator

--- src/Definitions/Theme/Theme.tsx
+++ src/Definitions/Theme/Theme.tsx
@@ -1,7 +1,5 @@
-import { createTheme } from "@mui/material/styles";
-import { ThemeProps } from "./Theme.interface";
-
-const drawerWidth = 250;
+import { createTheme, experimental_sx as sx } from "@mui/material/styles";
+import type {} from "@mui/lab/themeAugmentation";
 
 declare module "@mui/material/styles" {
   interface Palette {
@@ -17,7 +15,8 @@ declare module "@mui/material/styles" {
     subtitleBold: React.CSSProperties;
     bodySemiBold1: React.CSSProperties;
     bodySemiBold2: React.CSSProperties;
-    buttonBold: React.CSSProperties;
+    buttonBold1: React.CSSProperties;
+    buttonBold2: React.CSSProperties;
     buttonSemiBold: React.CSSProperties;
     captionSemiBold: React.CSSProperties;
   }
@@ -28,7 +27,8 @@ declare module "@mui/material/styles" {
     subtitleBold: React.CSSProperties;
     bodySemiBold1: React.CSSProperties;
     bodySemiBold2: React.CSSProperties;
-    buttonBold: React.CSSProperties;
+    buttonBold1: React.CSSProperties;
+    buttonBold2: React.CSSProperties;
     buttonSemiBold: React.CSSProperties;
     captionSemiBold?: React.CSSProperties;
   }
@@ -40,294 +40,221 @@ declare module "@mui/material/Typography" {
     subtitleBold: true;
     bodySemiBold1: true;
     bodySemiBold2: true;
-    buttonBold: true;
+    buttonBold1: true;
+    buttonBold2: true;
     buttonSemiBold: true;
     captionSemiBold: true;
   }
 }
 
 // Create a theme instance.
-export const baseTheme = (props: ThemeProps) =>
-  createTheme({
-    components: {
-      MuiButton: {
-        styleOverrides: {
-          root: {
-            textTransform: "none",
-          },
-        },
+export const baseTheme = createTheme({
+  components: {
+    MuiButton: {
+      defaultProps: {
+        disableElevation: true,
+        variant: "contained",
       },
-      MuiInputBase: {
-        styleOverrides: {
-          root: {
-            "&.MuiInput-root.Mui-disabled": {
-              ":before": {
-                borderBottomStyle: "solid",
-              },
-            },
-            fontSize: "14px",
-            "& > label > span": {
-              color: "red",
-            },
-            "& > label": {
-              color: "#221F20",
-              fontWeight: 600,
-              letterSpacing: "0.2px",
-            },
-          },
-        },
+      styleOverrides: {
+        root: sx({
+          textTransform: "none",
+          lineHeight: 1,
+          letterSpacing: 0.2,
+          borderRadius: 2,
+        }),
+        sizeSmall: sx({
+          px: 1.25,
+          py: 1.25,
+          typography: "buttonSemiBold",
+        }),
+        sizeMedium: sx({
+          px: 2,
+          py: 1.75,
+          typography: "buttonBold2",
+        }),
+        sizeLarge: sx({
+          px: 2.25,
+          py: 2,
+          typography: "buttonBold1",
+        }),
       },
-      MuiOutlinedInput: {
-        styleOverrides: {
-          root: {
-            ".MuiSelect-select.MuiSelect-select": {
-              fontSize: "14px",
-            },
-            ".css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input": {
-              fontSize: "14px",
-              height: "23px",
-              //setting font size and heigh when input outlined and medium size
-            },
-            ".css-1n4twyu-MuiInputBase-input-MuiOutlinedInput-input": {
-              height: "31px !important",
-              //setting font size and heigh when input outlined and small size
-            },
-            ".css-1ua80n0-MuiInputBase-input-MuiOutlinedInput-input": {
-              //setting height small input with start adorment
-              height: "31px !important",
-            },
-            ".css-1o9s3wi-MuiInputBase-input-MuiOutlinedInput-input": {
-              //setting height medium input with start adorment
-              height: "23px",
-            },
-            ".css-19qh8xo-MuiInputBase-input-MuiOutlinedInput-input": {
-              //setting height small input with end adorment
-              height: "31px !important",
-            },
-            ".css-nxo287-MuiInputBase-input-MuiOutlinedInput-input": {
-              //setting height medium input with end adorment
-              height: "23px",
-            },
-            ".css-10m3xgx-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.css-10m3xgx-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.css-10m3xgx-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input":
-              {
-                //change sizing medium select input
-                paddingTop: "14px",
-                paddingBottom: "14px",
-              },
-            ".css-17nr37i-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.css-17nr37i-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.css-17nr37i-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input":
-              {
-                //change sizing small select input
-                paddingTop: "18px",
-                paddingBottom: "18px",
-              },
-            ".css-dgem98-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.css-dgem98-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.css-dgem98-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input":
-              {
-                height: "23px",
-              },
-            ".css-p9abup-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.css-p9abup-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.css-p9abup-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input":
-              {
-                height: "31px",
-              },
-          },
-        },
+    },
+    MuiFormControlLabel: {
+      styleOverrides: {
+        label: ({ ownerState }) => ({
+          ...(ownerState.control.props.size === "small" && {
+            fontSize: "0.875rem",
+          }),
+        }),
       },
-      MuiFormControl: {
-        styleOverrides: {
-          root: {
-            "& > label > span": {
-              color: "red",
-            },
-            ".MuiInput-input": {
-              fontSize: "14px",
-            },
-            ".MuiSelect-nativeInput": {
-              border: "1px solid red",
-              color: "red",
-            },
-            ".css-16ulblx-MuiFormLabel-root-MuiInputLabel-root ": {
-              lineHeight: "30px",
-            },
-            ".css-1dyt4r5-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.MuiSelect-select":
-              {
-                fontSize: "14px",
-              },
-            "label + .css-j82al8-MuiInputBase-root-MuiInput-root": {
-              paddingBottom: "12px",
-            },
-            "label + .css-q6svtz-MuiInputBase-root-MuiInput-root": {
-              paddingBottom: "12px",
-            },
-            ".css-hd63pv-MuiSvgIcon-root-MuiSelect-icon": {
-              top: "calc(50% - 0.7em)", // change position icon arrow_down in select input
-            },
-            ".css-ittuaa-MuiInputAdornment-root": {
-              ".css-1b82i9m-MuiTypography-root": {
-                fontSize: "14px", // make adorment fontsize 14px in input
-              },
-            },
-          },
-        },
-      },
-      MuiDrawer: props.components?.MuiDrawer || {
-        styleOverrides: {
-          root: {
-            position: "relative",
-            width: drawerWidth,
-            flexShrink: 0,
-            "& .MuiDrawer-paper": {
-              width: drawerWidth,
-              boxSizing: "border-box",
-            },
+    },
+    MuiDrawer: {
+      styleOverrides: {
+        root: {
+          position: "relative",
+          width: 250,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: 250,
+            boxSizing: "border-box",
           },
         },
       },
     },
-    typography: {
-      fontFamily: props.replaceTheme?.typography?.fontFamily || "Inter",
-      htmlFontSize: 16,
-      fontSize: 14,
-      h1: {
-        fontSize: "3rem",
-        fontWeight: 700,
-        lineHeight: 1,
-      },
-      h2: {
-        fontSize: "2.5rem",
-        fontWeight: 700,
-        lineHeight: 1,
-      },
-      h3: {
-        fontSize: "2rem",
-        fontWeight: 700,
-        lineHeight: 1,
-      },
-      h4: {
-        fontSize: "1.5rem",
-        fontWeight: 700,
-        lineHeight: 1,
-      },
-      h5: {
-        fontSize: "1.25rem",
-        fontWeight: 700,
-        lineHeight: 1,
-      },
-      h6: {
-        fontSize: "1.125rem",
-        fontWeight: 700,
-        lineHeight: 1,
-      },
-      subtitle: {
-        fontSize: "1rem",
-        fontWeight: 400,
-        lineHeight: 1,
-      },
-      subtitleBold: {
-        fontSize: "1rem",
-        fontWeight: 700,
-        lineHeight: 1,
-      },
-      body1: {
-        fontSize: "1rem",
-        fontWeight: 400,
-        lineHeight: "24px",
-      },
-      body2: {
-        fontSize: "0.875rem",
-        fontWeight: 400,
-        lineHeight: "21px",
-      },
-      bodySemiBold1: {
-        fontSize: "1rem",
-        fontWeight: 600,
-        lineHeight: "24px",
-      },
-      bodySemiBold2: {
-        fontSize: "0.875rem",
-        fontWeight: 600,
-        lineHeight: "21px",
-      },
-      buttonBold: {
-        fontSize: "1rem",
-        fontWeight: 700,
-        lineHeight: 1,
-      },
-      buttonSemiBold: {
-        fontSize: "0.875rem",
-        fontWeight: 700,
-        lineHeight: 1,
-      },
-      caption: {
-        fontSize: "0.75rem",
-        fontWeight: 400,
-        lineHeight: 1,
-      },
-      captionSemiBold: {
-        fontSize: "0.75rem",
-        fontWeight: 600,
-        lineHeight: "19.2px",
-      },
-      overline: {
-        fontSize: "0.75rem",
-        fontWeight: 700,
-        lineHeight: 1,
-      },
+  },
+  spacing: 8,
+  typography: {
+    fontFamily: ['"Inter"', "sans-serif"].join(","),
+    htmlFontSize: 16,
+    fontSize: 14,
+    h1: {
+      fontSize: "3rem",
+      fontWeight: 700,
+      lineHeight: 1,
     },
-    palette: {
-      primary: {
-        main: "#DC3931",
-        dark: "#B02D27",
-        light: "#E3615A",
-      },
-      secondary: {
-        main: "#525252",
-        dark: "#262626",
-        light: "#737373",
-      },
-      info: {
-        main: "#3A82F6",
-        dark: "#1E4ED8",
-        light: "#629BF8",
-      },
-      error: {
-        main: "#F43F5E",
-        dark: "#BE123C",
-        light: "#FB7185",
-      },
-      warning: {
-        main: "#F59E0B",
-        dark: "#B45309",
-        light: "#FBBF24",
-      },
-      success: {
-        main: "#22C55E",
-        dark: "#15803D",
-        light: "#34D399",
-      },
-      grey: {
-        "50": "#FAFAFA",
-        "100": "#F5F5F5",
-        "200": "#E5E5E5",
-        "300": "#D4D4D4",
-        "400": "#A3A3A3",
-        "500": "#737373",
-        "600": "#525252",
-        "700": "#404040",
-        "800": "#262626",
-        "900": "#171717",
-      },
-      bluegrey: {
-        "50": "#F8FAFC",
-        "100": "#F1F5F9",
-        "200": "#E2E8F0",
-        "300": "#CBD5E1",
-        "400": "#94A3B8",
-        "500": "#64748B",
-        "600": "#475569",
-        "700": "#334155",
-        "800": "#1E293B",
-        "900": "#0F172A",
-      },
-      contrastThreshold: 3,
-      tonalOffset: 0.2,
+    h2: {
+      fontSize: "2.5rem",
+      fontWeight: 700,
+      lineHeight: 1,
     },
-  });
+    h3: {
+      fontSize: "2rem",
+      fontWeight: 700,
+      lineHeight: 1,
+    },
+    h4: {
+      fontSize: "1.5rem",
+      fontWeight: 700,
+      lineHeight: 1,
+    },
+    h5: {
+      fontSize: "1.25rem",
+      fontWeight: 700,
+      lineHeight: 1,
+    },
+    h6: {
+      fontSize: "1.125rem",
+      fontWeight: 700,
+      lineHeight: 1,
+    },
+    subtitle: {
+      fontSize: "1rem",
+      fontWeight: 400,
+      lineHeight: 1,
+    },
+    subtitleBold: {
+      fontSize: "1rem",
+      fontWeight: 700,
+      lineHeight: 1,
+    },
+    body1: {
+      fontSize: "1rem",
+      fontWeight: 400,
+      lineHeight: "24px",
+    },
+    body2: {
+      fontSize: "0.875rem",
+      fontWeight: 400,
+      lineHeight: "21px",
+    },
+    bodySemiBold1: {
+      fontSize: "1rem",
+      fontWeight: 600,
+      lineHeight: "24px",
+    },
+    bodySemiBold2: {
+      fontSize: "0.875rem",
+      fontWeight: 600,
+      lineHeight: "21px",
+    },
+    buttonBold1: {
+      fontSize: "1.125rem",
+      fontWeight: 700,
+      lineHeight: 1,
+    },
+    buttonBold2: {
+      fontSize: "1rem",
+      fontWeight: 700,
+      lineHeight: 1,
+    },
+    buttonSemiBold: {
+      fontSize: "0.875rem",
+      fontWeight: 600,
+      lineHeight: 1,
+    },
+    caption: {
+      fontSize: "0.75rem",
+      fontWeight: 400,
+      lineHeight: 1,
+    },
+    captionSemiBold: {
+      fontSize: "0.75rem",
+      fontWeight: 600,
+      lineHeight: "19.2px",
+    },
+    overline: {
+      fontSize: "0.75rem",
+      fontWeight: 700,
+      lineHeight: 1,
+    },
+  },
+  palette: {
+    primary: {
+      main: "#DC3931",
+      dark: "#B02E27",
+      light: "#E3615A",
+    },
+    secondary: {
+      main: "#525252",
+      dark: "#262626",
+      light: "#737373",
+    },
+    info: {
+      main: "#3A82F6",
+      dark: "#1E4ED8",
+      light: "#629BF8",
+    },
+    error: {
+      main: "#F43F5E",
+      dark: "#BE123C",
+      light: "#FB7185",
+    },
+    warning: {
+      main: "#F59E0B",
+      dark: "#B45309",
+      light: "#FBBF24",
+      contrastText: "#FFFFFF",
+    },
+    success: {
+      main: "#22C55E",
+      dark: "#15803D",
+      light: "#34D399",
+    },
+    grey: {
+      "50": "#FAFAFA",
+      "100": "#F5F5F5",
+      "200": "#E5E5E5",
+      "300": "#D4D4D4",
+      "400": "#A3A3A3",
+      "500": "#737373",
+      "600": "#525252",
+      "700": "#404040",
+      "800": "#262626",
+      "900": "#171717",
+    },
+    bluegrey: {
+      "50": "#F8FAFC",
+      "100": "#F1F5F9",
+      "200": "#E2E8F0",
+      "300": "#CBD5E1",
+      "400": "#94A3B8",
+      "500": "#64748B",
+      "600": "#475569",
+      "700": "#334155",
+      "800": "#1E293B",
+      "900": "#0F172A",
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+});
