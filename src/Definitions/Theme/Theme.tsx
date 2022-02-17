185cd5a4cbb910d25617cf506b78873749b82bd1
gilangdarmawan <jobs.gilangdarmawan@gmail.com>
2022-02-18 00:32:59 +0700

    Update theme (palette, component, typography)

--- src/Definitions/Theme/Theme.tsx
+++ src/Definitions/Theme/Theme.tsx
@@ -77,6 +77,7 @@ export const baseTheme = createTheme({
           lineHeight: 1,
           letterSpacing: 0.2,
           borderRadius: 2,
+          height: "100%",
         }),
         sizeSmall: sx({
           px: 1.25,
@@ -92,6 +93,21 @@ export const baseTheme = createTheme({
           px: 2.25,
           py: 2,
           typography: "buttonBold1",
+        }),
+        iconSizeSmall: sx({
+          "& > *:first-child": {
+            typography: "buttonSemiBold",
+          },
+        }),
+        iconSizeMedium: sx({
+          "& > *:first-child": {
+            typography: "buttonBold2",
+          },
+        }),
+        iconSizeLarge: sx({
+          "& > *:first-child": {
+            typography: "buttonBold1",
+          },
         }),
       },
     },
@@ -440,10 +456,10 @@ export const baseTheme = createTheme({
             color: "red",
           },
         },
-        marginNormal: {
-          marginTop: "0px",
-          marginBottom: "12px",
-        },
+        marginNormal: sx({
+          mt: 0,
+          mb: 2,
+        }),
       },
     },
     MuiDrawer: {
@@ -457,6 +473,25 @@ export const baseTheme = createTheme({
             boxSizing: "border-box",
           },
         },
+      },
+    },
+    MuiTimelineDot: {
+      styleOverrides: {
+        root: sx({
+          p: 0,
+          mx: 0,
+          my: 1,
+          border: "none",
+        }),
+      },
+    },
+    MuiTimelineConnector: {
+      styleOverrides: {
+        root: sx({
+          backgroundColor: "transparent",
+          border: "1px dashed",
+          borderColor: "divider",
+        }),
       },
     },
   },
@@ -557,6 +592,7 @@ export const baseTheme = createTheme({
     },
   },
   palette: {
+    divider: "#E0E0E0",
     primary: {
       main: "#DC3931",
       dark: "#B02E27",
@@ -587,6 +623,12 @@ export const baseTheme = createTheme({
       main: "#22C55E",
       dark: "#15803D",
       light: "#4ED17E",
+      contrastText: "#FFFFFF",
+    },
+    text: {
+      primary: "#221F20",
+      secondary: "#64748B",
+      disabled: "#999999",
     },
     grey: {
       "50": "#FAFAFA",
