2912dc36e33d7624119ad17740fa0fbb2c5eef24
Muhammad Farhan <muhammadfarhan@Muhammads-MacBook-Pro.local>
2022-02-17 19:03:31 +0700

    change import

--- src/Components/Molecules/FMCard/index.tsx
+++ src/Components/Molecules/FMCard/index.tsx
@@ -1,12 +1,13 @@
 import React, { memo, useState } from "react";
+import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
+import Collapse from "@mui/material/Collapse";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 
+import { FAIcon } from "@Atoms/Icon";
 import { IFMCard } from "./FMCard.interfaces";
 import { FATypography } from "@Atoms/FATypography";
-import { Box, Collapse } from "@mui/material";
-import { FAIcon } from "@Atoms/Icon";
 
 export const FMCard = memo((props: IFMCard) => {
   const {
