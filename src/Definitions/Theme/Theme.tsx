2847b7d62ed9ec76bca37b9e5fca7ed4f1c67b5f
gilangdarmawan <jobs.gilangdarmawan@gmail.com>
2022-02-21 18:05:03 +0700

    Add shadows, paper, timeline, and stepper theme component

--- src/Definitions/Theme/Theme.tsx
+++ src/Definitions/Theme/Theme.tsx
@@ -49,17 +49,6 @@ declare module "@mui/material/Typography" {
 // Create a theme instance.
 export const baseTheme = createTheme({
   components: {
-    MuiTableCell: {
-      styleOverrides: {
-        root: {
-          borderBottom: "1px solid",
-          borderColor: "#E0E0E0",
-        },
-        head: sx({
-          typography: "bodySemiBold2",
-        }),
-      },
-    },
     MuiPaginationItem: {
       styleOverrides: {
         sizeLarge: sx({
@@ -497,26 +486,153 @@ export const baseTheme = createTheme({
         },
       },
     },
-    MuiTimelineDot: {
+    MuiPaper: {
       styleOverrides: {
         root: sx({
-          p: 0,
-          mx: 0,
-          my: 1,
-          border: "none",
+          borderRadius: 2,
+          "&.flex-paper-0": {
+            p: 0,
+          },
+          "&.flex-paper-1": {
+            p: 1,
+          },
+          "&.flex-paper-2": {
+            p: 2,
+          },
         }),
+      },
+    },
+    MuiStepConnector: {
+      styleOverrides: {
+        line: sx({
+          borderColor: "grey.300",
+        }),
+      },
+    },
+    MuiStepIcon: {
+      styleOverrides: {
+        root: {
+          height: 32,
+          width: 32,
+          color: "transparent",
+        },
+      },
+    },
+    MuiStepLabel: {
+      styleOverrides: {
+        label: sx({
+          typography: "body2",
+          "&.Mui-active": {
+            typography: "bodySemiBold2",
+          },
+        }),
+        iconContainer: sx({
+          mr: 1,
+          pr: 0,
+          height: 32,
+          width: 32,
+          alignItems: "center",
+          justifyContent: "center",
+          border: "1px solid",
+          borderRadius: "50%",
+          borderColor: "grey.300",
+          text: {
+            fill: "#221F20",
+          },
+          "& .Mui-active": {
+            borderColor: "primary.main",
+            text: {
+              fill: "#FFFFFF",
+              fontWeight: 600,
+            },
+          },
+        }),
+      },
+    },
+    MuiTable: {
+      styleOverrides: {
+        root: {
+          tableLayout: "fixed",
+        },
+      },
+    },
+    MuiTableCell: {
+      styleOverrides: {
+        root: sx({
+          borderBottom: "1px solid",
+          borderColor: "divider",
+        }),
+        head: sx({
+          typography: "bodySemiBold2",
+        }),
+      },
+    },
+    MuiTablePagination: {
+      styleOverrides: {
+        root: {
+          borderBottom: "none",
+        },
       },
     },
     MuiTimelineConnector: {
       styleOverrides: {
         root: sx({
+          position: "absolute",
+          top: "calc(50% + 11.5px)",
+          height: "calc(50% - 5.75px)",
           backgroundColor: "transparent",
           border: "1px dashed",
           borderColor: "divider",
         }),
       },
     },
+    MuiTimelineDot: {
+      styleOverrides: {
+        root: sx({
+          px: 0,
+          mx: 0,
+          border: "none",
+        }),
+      },
+    },
+    MuiTimelineItem: {
+      styleOverrides: {
+        root: sx({
+          "&::before": {
+            flex: "none",
+            p: 0,
+          },
+        }),
+      },
+    },
   },
+  shadows: [
+    "none",
+    "0px 0px 16px rgba(0, 0, 0, 0.1)",
+    "0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 1px 3px rgba(0, 0, 0, 0.12)",
+    "0px 3px 3px -2px rgba(0,0,0,0.2),0px 3px 4px 0px rgba(0,0,0,0.14),0px 1px 8px 0px rgba(0,0,0,0.12)",
+    "0px 2px 8px 1px rgba(0, 0, 0, 0.16)",
+    "0px 3px 5px -1px rgba(0,0,0,0.2),0px 5px 8px 0px rgba(0,0,0,0.14),0px 1px 14px 0px rgba(0,0,0,0.12)",
+    "0px 3px 5px -1px rgba(0,0,0,0.2),0px 6px 10px 0px rgba(0,0,0,0.14),0px 1px 18px 0px rgba(0,0,0,0.12)",
+    "0px 4px 5px -2px rgba(0,0,0,0.2),0px 7px 10px 1px rgba(0,0,0,0.14),0px 2px 16px 1px rgba(0,0,0,0.12)",
+    "0px 5px 5px -3px rgba(0,0,0,0.2),0px 8px 10px 1px rgba(0,0,0,0.14),0px 3px 14px 2px rgba(0,0,0,0.12)",
+    "0px 5px 6px -3px rgba(0,0,0,0.2),0px 9px 12px 1px rgba(0,0,0,0.14),0px 3px 16px 2px rgba(0,0,0,0.12)",
+    "0px 6px 6px -3px rgba(0,0,0,0.2),0px 10px 14px 1px rgba(0,0,0,0.14),0px 4px 18px 3px rgba(0,0,0,0.12)",
+    "0px 6px 7px -4px rgba(0,0,0,0.2),0px 11px 15px 1px rgba(0,0,0,0.14),0px 4px 20px 3px rgba(0,0,0,0.12)",
+    "0px 7px 8px -4px rgba(0,0,0,0.2),0px 12px 17px 2px rgba(0,0,0,0.14),0px 5px 22px 4px rgba(0,0,0,0.12)",
+    "0px 7px 8px -4px rgba(0,0,0,0.2),0px 13px 19px 2px rgba(0,0,0,0.14),0px 5px 24px 4px rgba(0,0,0,0.12)",
+    "0px 7px 9px -4px rgba(0,0,0,0.2),0px 14px 21px 2px rgba(0,0,0,0.14),0px 5px 26px 4px rgba(0,0,0,0.12)",
+    "0px 8px 9px -5px rgba(0,0,0,0.2),0px 15px 22px 2px rgba(0,0,0,0.14),0px 6px 28px 5px rgba(0,0,0,0.12)",
+    "0px 8px 10px -5px rgba(0,0,0,0.2),0px 16px 24px 2px rgba(0,0,0,0.14),0px 6px 30px 5px rgba(0,0,0,0.12)",
+    "0px 8px 11px -5px rgba(0,0,0,0.2),0px 17px 26px 2px rgba(0,0,0,0.14),0px 6px 32px 5px rgba(0,0,0,0.12)",
+    "0px 9px 11px -5px rgba(0,0,0,0.2),0px 18px 28px 2px rgba(0,0,0,0.14),0px 7px 34px 6px rgba(0,0,0,0.12)",
+    "0px 9px 12px -6px rgba(0,0,0,0.2),0px 19px 29px 2px rgba(0,0,0,0.14),0px 7px 36px 6px rgba(0,0,0,0.12)",
+    "0px 10px 13px -6px rgba(0,0,0,0.2),0px 20px 31px 3px rgba(0,0,0,0.14),0px 8px 38px 7px rgba(0,0,0,0.12)",
+    "0px 10px 13px -6px rgba(0,0,0,0.2),0px 21px 33px 3px rgba(0,0,0,0.14),0px 8px 40px 7px rgba(0,0,0,0.12)",
+    "0px 10px 14px -6px rgba(0,0,0,0.2),0px 22px 35px 3px rgba(0,0,0,0.14),0px 8px 42px 7px rgba(0,0,0,0.12)",
+    "0px 11px 14px -7px rgba(0,0,0,0.2),0px 23px 36px 3px rgba(0,0,0,0.14),0px 9px 44px 8px rgba(0,0,0,0.12)",
+    "0px 11px 15px -7px rgba(0,0,0,0.2),0px 24px 38px 3px rgba(0,0,0,0.14),0px 9px 46px 8px rgba(0,0,0,0.12)",
+  ],
   spacing: 8,
   typography: {
     fontFamily: ['"Inter"', "sans-serif"].join(","),
@@ -614,6 +730,10 @@ export const baseTheme = createTheme({
     },
   },
   palette: {
+    common: {
+      black: "#000",
+      white: "#fff",
+    },
     divider: "#E0E0E0",
     primary: {
       main: "#DC3931",
