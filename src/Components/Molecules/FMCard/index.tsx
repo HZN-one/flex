ee0a0e04e3e10b8a5549f0a7cf83c3af1cfb2c4f
Muhammad Farhan <muhammadfarhan@Muhammads-MacBook-Pro.local>
2022-02-23 09:48:34 +0700

    rollback feature fmcard collapse

--- src/Components/Molecules/FMCard/index.tsx
+++ src/Components/Molecules/FMCard/index.tsx
@@ -1,66 +1,31 @@
-import React, { memo, useState } from "react";
-import Box from "@mui/material/Box";
+import React, { memo } from "react";
 import Card from "@mui/material/Card";
-import Collapse from "@mui/material/Collapse";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 
-import { FAIcon } from "@Atoms/Icon";
 import { IFMCard } from "./FMCard.interfaces";
 import { FATypography } from "@Atoms/FATypography";
-import { FAIconButton } from "@Atoms/FAIconButton";
 
 export const FMCard = memo((props: IFMCard) => {
-  const {
-    testID,
-    actions,
-    title,
-    children,
-    isCollapsible,
-    ...materialUIProps
-  } = props;
-
-  const [isOpen, setIsOpen] = useState(true);
+  const { testID, actions, title, children, ...materialUIProps } = props;
   return (
     <Card data-testid={testID} {...materialUIProps}>
       <CardContent>
         {title && (
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              marginBottom: isOpen ? 3.75 : 0,
-              alignItems: "center",
-            }}
-          >
+          <>
             <FATypography
               testID="typography-card"
               variant="h6"
               fontWeight={700}
+              sx={{ marginBottom: 3.75 }}
             >
               {title}
             </FATypography>
-            {isCollapsible && (
-              <FAIconButton
-                testID="icon-button-arrow-collapse"
-                onClick={() => {
-                  setIsOpen(!isOpen);
-                }}
-              >
-                <FAIcon testID="icon-arrow-collapse">arrow_drop_down</FAIcon>
-              </FAIconButton>
-            )}
-          </Box>
+          </>
         )}
-        <Collapse in={isOpen} timeout="auto" unmountOnExit>
-          {children}
-        </Collapse>
+        {children}
       </CardContent>
-      {actions && (
-        <Collapse in={isOpen} timeout="auto" unmountOnExit>
-          <CardActions>{actions}</CardActions>
-        </Collapse>
-      )}
+      {actions && <CardActions>{actions}</CardActions>}
     </Card>
   );
 });
