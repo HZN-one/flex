806686f5e802718b9d7da15facb6419cfbef9dfd
Muhammad Farhan <muhammadfarhan@Muhammads-MacBook-Pro.local>
2022-02-24 21:07:07 +0700

    change interface label value

--- src/Components/Molecules/FMLabelValue/FMLabelValue.interface.tsx
+++ src/Components/Molecules/FMLabelValue/FMLabelValue.interface.tsx
@@ -1,8 +1,8 @@
 import { IFATypography } from "@Atoms/FATypography/FATypography.interfaces";
 
 export interface IFMLabelValue {
-  label: string;
-  value?: string;
+  label: string | number;
+  value?: string | number;
   propLabel?: Omit<IFATypography, "TestID">;
   propValue?: Omit<IFATypography, "TestID">;
   margin?: "normal" | "none";
