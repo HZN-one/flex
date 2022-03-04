94e77577b8d1fd71340afa436e253402d6c3a488
Bayu Pranoto Dwi Putra <pranoto@Bayus-MacBook-Air.local>
2022-03-04 10:25:39 +0700

    fix FMLabelValue interface types

--- src/Components/Molecules/FMLabelValue/FMLabelValue.interface.tsx
+++ src/Components/Molecules/FMLabelValue/FMLabelValue.interface.tsx
@@ -2,7 +2,7 @@ import { IFATypography } from "@Atoms/FATypography/FATypography.interfaces";
 
 export interface IFMLabelValue {
   label: string;
-  value?: string;
+  value?: string | number;
   propLabel?: Omit<IFATypography, "TestID">;
   propValue?: Omit<IFATypography, "TestID">;
   margin?: "normal" | "none";
