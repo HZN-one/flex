005f1e4376521133eec51e5db9c3f3ea9bc70737
bryantobing12 <bryantobing0@gmail.com>
2022-03-09 14:54:49 +0700

    Update FAButton to receive isLoading props

--- src/Components/Atoms/FAButton/index.tsx
+++ src/Components/Atoms/FAButton/index.tsx
@@ -1,12 +1,28 @@
 import React, { memo } from "react";
 import { Button } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+
+import { FAIcon } from "@Atoms/FAIcon";
 
 import { IFAButtonProps } from "./FAButton.interface";
 
 export const FAButton = memo((props: IFAButtonProps) => {
-  const { testID, ...materialUIProps } = props;
+  const { testID, isLoading, ...materialUIProps } = props;
 
-  return <Button data-testid={testID} {...materialUIProps} />;
+  return (
+    <>
+      {!isLoading ? (
+        <Button data-testid={testID} {...materialUIProps} />
+      ) : (
+        <LoadingButton
+          loadingPosition="start"
+          startIcon={<FAIcon testID="icon-save">loading</FAIcon>}
+          loading
+          {...materialUIProps}
+        />
+      )}
+    </>
+  );
 });
 
 FAButton.displayName = "FAButton";
