9f640b0dfd71936a9f5c25100ce8f7b09e3d5b20
Muhammad Farhan <muhammadfarhan@Muhammads-MacBook-Pro.local>
2022-04-18 09:53:02 +0700

    create component bar chart

---
+++ src/Components/Molecules/FMBarChart/FMBarChart.inteface.ts
@@ -0,0 +1,14 @@
+import type { ApexOptions } from "apexcharts";
+
+export interface IFMBarChart {
+  testID: `bar-chart-${string}`;
+  chartOptions?: ApexOptions;
+  width?: string | number;
+  height?: string | number;
+  categories: string[];
+  colors?: string[];
+  chartData: number[];
+  minValue?: number;
+  maxValue?: number;
+  tickAmount?: number;
+}
---
+++ src/Components/Molecules/FMBarChart/index.tsx
@@ -0,0 +1,91 @@
+import React, { memo } from "react";
+
+import Chart from "react-apexcharts";
+
+import type { ApexOptions } from "apexcharts";
+import { IFMBarChart } from "./FMBarChart.inteface";
+
+export const FMBarChart = memo((props: IFMBarChart) => {
+  const chartOptions: ApexOptions = {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+      fontFamily: "inter",
+      zoom: {
+        enabled: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+        distributed: true,
+      },
+    },
+    tooltip: {
+      enabled: false,
+    },
+    grid: {
+      show: true,
+      xaxis: {
+        lines: {
+          show: true,
+        },
+      },
+      yaxis: {
+        lines: {
+          show: true,
+        },
+      },
+    },
+    legend: {
+      show: false,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: props.categories,
+      min: props.minValue,
+      max: props.maxValue,
+      tickAmount: props.tickAmount,
+      labels: {
+        style: {
+          colors: "#64748B",
+          fontSize: "12px",
+        },
+      },
+    },
+    yaxis: {
+      labels: {
+        align: "left",
+        style: {
+          colors: "#221F20",
+          fontSize: "12px",
+        },
+      },
+    },
+    colors: props.colors,
+    ...props.chartOptions,
+  };
+
+  const chartData = [
+    {
+      data: props.chartData,
+    },
+  ];
+
+  return (
+    <>
+      <Chart
+        data-testid={props.testID}
+        options={chartOptions}
+        series={chartData}
+        type="bar"
+        width={props.width}
+        height={props.height}
+      />
+    </>
+  );
+});
+FMBarChart.displayName = "FMBarChart";
